7bf5c02d9a817ec6a9af793d5a9c10e29f3d0b5b
devabacus <32705417+devabacus@users.noreply.github.com>
2025-06-13 11:30:06 +0500

    1

--- t9s_flutter/lib/check/server_check_ui.dart
+++ t9s_flutter/lib/check/server_check_ui.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:serverpod_flutter/serverpod_flutter.dart';
 import 'package:t9s_client/t9s_client.dart';
 
-var client = Client('https://api.my-points.ru/')
+var client = Client('https://api2.my-points.ru/')
 // var client = Client('http://localhost:8080/')
   ..connectivityMonitor = FlutterConnectivityMonitor();
 
